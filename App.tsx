663591277c68d428ab12bfd8d9058404a6585f2d
suvo-gh <hazra.sghosh@gmail.com>
2023-12-05 00:52:20 +0530

    hello world

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  View,
+  SafeAreaView,
+  Text
+} from 'react-native';
+
+function App() {
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>
+          Hello World!
+        </Text>
+        <Text>
+          Hello World!
+        </Text>
+        <Text>
+          Hello World!
+        </Text>
+        <Text>
+          Hello World!
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App;
