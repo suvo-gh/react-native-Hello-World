843163eea4c8a5bc03dac09251e9c9b2fc556d42
suvo-gh <hazra.sghosh@gmail.com>
2023-12-05 01:53:41 +0530

    dark and light mode implementation

--- App.tsx
+++ App.tsx
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   SafeAreaView,
   Text,
   Button,
-  Alert
+  Alert,
+  StyleSheet,
+  useColorScheme,
 } from 'react-native';
 
 function App() {
+  
+  let isDarkMode = useColorScheme() === 'dark';
+
   return(
-    <SafeAreaView>
-      <View>
-        <Text>
+      <View style={isDarkMode ? styles.blackContainer : styles.whiteContainer}>
+        <Text style={isDarkMode ? styles.blackText : styles.whiteText}>
           Hello World!
         </Text>
-        <Text>
-          Hello World!
-        </Text>
-        <Text>
-          Hello World!
-        </Text>
-        <Text>
-          Hello World!
-        </Text>
-        <Button
-        title="Press me"
-        onPress={() => Alert.alert('Cannot press this one')}
-      />
       </View>
-    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  whiteContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black'
+
+  },
+  blackContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  blackText: {
+    color: '#000000',
+    backgroundColor: "white"
+  },
+  whiteText: {
+    color: "#FFFFFF",
+    backgroundColor: "black"
+  }
+})
 
 export default App;
